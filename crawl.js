3369f481c4c988a15a925f8dae52a02fc19d1d62
ayush tripathi <ayushtripathi9794@gmail.com>
2023-04-06 00:11:39 +0530

    refactor: code cleanup

--- crawl.js
+++ crawl.js
@@ -23,7 +23,7 @@ const normalizeUrl = (url) => {
 
         return pureUrl;
     } catch (e) {
-        console.error(e);
+        console.error(e.message);
         return null;
     }
 }
@@ -47,7 +47,6 @@ const filterFalsyUrls = (urls) => {
 }
 
 const getUrlsFromHtml = (htmlBody, baseUrl) => {
-
 
     if (!baseUrl) {
         throw new Error('Base URL is required.');
@@ -74,7 +73,7 @@ const getUrlsFromHtml = (htmlBody, baseUrl) => {
 }
 
 const printCrawling = (url) => {
-    console.log(`Crawling ${url}...`);
+    console.log(`Crawling ${url} ...`);
 }
 
 const hasSameDomain = (url, baseUrl) => {
@@ -84,6 +83,15 @@ const hasSameDomain = (url, baseUrl) => {
     return urlObj.host === baseUrlObj.host;
 }
 
+const handlePageResponseError = (pageResponse, currentUrl) => {
+    if (pageResponse.status !== 200) {
+        throw new Error(`Failed to fetch ${currentUrl}`);
+    }
+
+    if (!pageResponse.headers.get('content-type').includes('text/html')) {
+        throw new Error(`Page ${currentUrl} is not HTML`);
+    }
+}
 const crawlPage = async (baseUrl, currentUrl = baseUrl, pages = {}) => {
     try {
 
@@ -105,13 +113,7 @@ const crawlPage = async (baseUrl, currentUrl = baseUrl, pages = {}) => {
 
         const pageResponse = await fetch(normalizedCurrentUrl);
 
-        if (pageResponse.status !== 200) {
-            throw new Error(`Failed to fetch ${normalizedCurrentUrl}`);
-        }
-
-        if (!pageResponse.headers.get('content-type').includes('text/html')) {
-            throw new Error(`Page ${normalizedCurrentUrl} is not HTML`);
-        }
+        handlePageResponseError(pageResponse, normalizedCurrentUrl);
 
         const htmlBody = await pageResponse.text();
 
@@ -120,15 +122,14 @@ const crawlPage = async (baseUrl, currentUrl = baseUrl, pages = {}) => {
 
         if (urls.length === 0) {
             return pages;
-
-        } else {
-
-            const promises = urls.map((url) => crawlPage(normalizedBaseUrl, normalizeUrl(url), pages));
-
-            (await Promise.all(promises))
-
-            return pages;
         }
+
+        const promises = urls.map((url) => crawlPage(normalizedBaseUrl, normalizeUrl(url), pages));
+
+        (await Promise.all(promises))
+
+        return pages;
+
     } catch (e) {
         console.dir(e.message);
         return pages;
